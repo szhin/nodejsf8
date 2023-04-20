ffeaf26a02e4a0e22c396856641b6cc72b4cf235
bangpham <uzumakinarutoshin@gmail.com>
2023-04-20 19:26:39 +0700

    Refactor code: create routes, app/controller folders

---
+++ src/app/controllers/ViewsController.js
@@ -0,0 +1,15 @@
+
+class ViewsController {
+
+    // [GET] /news
+    index(req, res) {
+        res.render("news");
+    }
+
+    show(req, res) {
+        res.send("NEWS DETAIL !!!")
+    }
+
+}
+
+module.exports = new ViewsController();
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const handlebars = require("express-handlebars").engine;
 const app = express();
 const port = 3001;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
 
 // Xử lí khi GET và POST khi code nodejs
@@ -25,25 +27,8 @@ app.engine(".hbs", handlebars({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-app.get("/news", (req, res) => {
-  res.render("news");
-});
-app.get("/search", (req, res) => {
-  res.render("search");
-});
-app.post("/search", (req, res) => {
-  // nếu ko gọi app.use(
-  //   express.urlencoded({
-  //     extended: true,
-  //   }),
-  // );
-  // thì sẽ console.log ra undefined
-  console.log(req.body);
-  res.send("");
-});
+// Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
