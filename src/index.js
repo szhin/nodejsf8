afe921fca3fef79e2978bf4900847420d59c7a9b
bangpham <uzumakinarutoshin@gmail.com>
2023-04-20 10:00:53 +0700

    POST method

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
+const { log } = require("console");
 const handlebars = require("express-handlebars").engine;
 const app = express();
 const port = 3001;
 
 app.use(express.static(path.join(__dirname, "public")));
+
+// Xử lí khi GET và POST khi code nodejs
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+// Xử lí khi GET và POST khi code javascript (fetch, axios)
+app.use(express.json());
 
 // HTTP logger
 app.use(morgan("combined"));
@@ -24,9 +34,16 @@ app.get("/news", (req, res) => {
 app.get("/search", (req, res) => {
   res.render("search");
 });
-// app.post("/search", (req, res) => {
-//   res.render("search");
-// });
+app.post("/search", (req, res) => {
+  // nếu ko gọi app.use(
+  //   express.urlencoded({
+  //     extended: true,
+  //   }),
+  // );
+  // thì sẽ console.log ra undefined
+  console.log(req.body);
+  res.send("");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
