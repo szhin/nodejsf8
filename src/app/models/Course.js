6ee720a376652668a9613eb95ccaef6ba2b0db7a
bangpham <uzumakinarutoshin@gmail.com>
2023-04-21 14:24:18 +0700

    Course detail page

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -5,6 +5,8 @@ const Course = new Schema({
     name: { type: String, maxLength: 255 },
     description: { type: String, maxLength: 600 },
     image: { type: String, maxLength: 255 },
+    slug: { type: String },
+    videoId: { type: String },
     createAt: { type: Date, default: Date.now },
     updateAt: { type: Date, default: Date.now },
 });
