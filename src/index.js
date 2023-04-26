58251666b4748f2ee3e86183bafdc199888434cc
bangpham <uzumakinarutoshin@gmail.com>
2023-04-26 08:58:01 +0700

    Middleware for sort

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars').engine;
 
+const SortMiddleware = require('./app/middlewares/SortMiddleware').default;
+
 const route = require('./routes');
 const db = require('./config/db');
 
@@ -20,20 +22,8 @@ app.use(express.json());
 // Edit database, Update course
 app.use(methodOverride('_method'));
 
-// Áp dụng middleware cho toàn bộ website
-// app.use(bacBaoVe);
-
-// function bacBaoVe(req, res, next) {
-//     if (['vethuong', 'vevip'].includes(req.query.ve)) {
-//         req.face = 'Gach gach gach';
-//         return next();
-//     }
-
-//     res.status(403).json({
-//         message: 'Access denied',
-//         face: req.face,
-//     });
-// }
+// Custom middlewares
+app.use(SortMiddleware);
 
 // HTTP logger
 app.use(morgan('combined'));
@@ -45,11 +35,32 @@ app.engine(
         extname: '.hbs',
         helpers: {
             sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+              const sortType = field === sort.column ? sort.type :'default';
+
+              const icons = {
+                  default: 'oi oi-elevator',
+                  asc: 'oi oi-sort-ascending',
+                  desc: 'oi oi-sort-descending',
+              };
+              const types = {
+                default: 'desc',
+                asc: 'desc',
+                desc: 'asc',
+              }
+
+              const icon = icons[sortType]
+              const type = types[sortType]
+
+              return `<a href="?_sort&column=${field}&type=${type}">
+                        <span class="${icon}"></span>
+                    </a>`;
+            },
         },
     }),
 );
+
 app.set('view engine', '.hbs');
-// đổi / thành , để trên mọi hệ đề hành, join nó sẽ tự điền
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Route init
